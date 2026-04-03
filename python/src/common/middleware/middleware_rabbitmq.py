2cadfcd8c6d29466c09f12442e7e4a413099dbc0
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-04-03 03:00:30 -0300

    feat(queue): listen to msg and close

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -15,16 +15,28 @@ class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
     def start_consuming(
         self, on_message_callback: Callable[[bytes, Callable, Callable], None]
     ) -> None:
-        pass
+        def callback(chan, method, properties, body):
+            on_message_callback(
+                body, lambda: self._ack(chan, method), lambda: self._nack(chan, method)
+            )
+
+        self.chan.basic_consume(queue=self.queue_name, on_message_callback=callback)
+        self.chan.start_consuming()
 
     def stop_consuming(self) -> None:
-        pass
+        self.chan.stop_consuming()
 
     def send(self, message: bytes) -> None:
-        pass
+        self.chan.basic_publish(exchange="", routing_key=self.queue_name, body=message)
 
     def close(self) -> None:
         self.conn.close()
+
+    def _ack(self, chan, method) -> None:
+        chan.basic_ack(delivery_tag=method.delivery_tag)
+
+    def _nack(self, chan, method) -> None:
+        chan.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
 
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
