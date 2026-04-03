76ac96e4781444758995121cd57fc0b3ede45376
Alejo Ordoñez <alejoordonez02@gmail.com>
2026-04-03 02:58:53 -0300

    feat(queue): init and close

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -1,14 +1,32 @@
-import pika
-import random
-import string
-from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from typing import Callable
+
+from pika import BlockingConnection, ConnectionParameters
+
+from .middleware import MessageMiddlewareExchange, MessageMiddlewareQueue
+
 
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
+    def __init__(self, host: str, queue_name: str):
+        self.queue_name = queue_name
+        self.conn = BlockingConnection(ConnectionParameters(host))
+        self.chan = self.conn.channel()
+        self.chan.queue_declare(queue=self.queue_name)
 
-    def __init__(self, host, queue_name):
+    def start_consuming(
+        self, on_message_callback: Callable[[bytes, Callable, Callable], None]
+    ) -> None:
         pass
 
+    def stop_consuming(self) -> None:
+        pass
+
+    def send(self, message: bytes) -> None:
+        pass
+
+    def close(self) -> None:
+        self.conn.close()
+
+
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
-    
     def __init__(self, host, exchange_name, routing_keys):
         pass
